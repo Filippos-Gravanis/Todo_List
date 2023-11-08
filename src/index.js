5bc722c6925a3d95e440c7952d4a43fd4a9e2721
Filippos Gravanis <grampouni20@gmail.com>
2023-11-08 13:43:31 +0200

    d

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 import "./styles.css"
 class todo {
-    constructor (title,description,date){
+    constructor (title,description,duedate,priority){
+        this.id = Date.now()+ Math.floor( Math.random() * 100000)
         this.title = title
         this.description = description
-        this.date = date
+        this.duedate = duedate
+        this.priority = priority
     }
 }
+
+class todos {
+    constructor (){
+        this.todoItems= []
+    }
+
+    addTodo(todo){
+        this.todoItems.push(todo)
+    }
+
+    deleteTodo(todo){
+        let todoToDelete = new todoFinder(todo,this.todoItems)
+        this.todoItems.splice(todoToDelete.find(),1)
+        console.log(this.todoItems)
+    }
+}
+
+class todoFinder {
+    constructor (todo,todos){
+        this.todo = todo
+        this.todos = todos
+    }
+    
+    find(){
+    let index = 0
+    this.todos.forEach(listTodo => {
+        
+        if (listTodo.id ==this.todo.id){
+            index = this.todos.indexOf(listTodo)
+        }
+    })
+    return index
+}
+    
+}
+
+
+let todos = new todos()
 
 
 function header () {
@@ -22,24 +62,53 @@ header()
 
 
 function Sidebar () {
+    let selectedItem = 'All'
     const sidebarContainer = document.createElement('div')
     sidebarContainer.classList.add('sidebar')
-    document.querySelector('.pageContainer').appendChild(sidebarContainer)
-    createNavItem("All")
-    createNavItem("Today")
-    createNavItem("This Week")
-    createNavItem("This Week")
+    console.log(sidebarContainer);
+    sidebarContainer.appendChild(createNavItem("All"))
+    sidebarContainer.appendChild(createNavItem("Today"))
+    sidebarContainer.appendChild(createNavItem("This Week"))
+    return sidebarContainer
 }
 
 
 function createNavItem (text) {
-    const sidebar = document.querySelector('.sidebar')
     const navItem = document.createElement('div')
     navItem.textContent = text
     navItem.classList.add('navItem')
-    sidebar.appendChild(navItem)
+    navItem.addEventListener('click',(ev)=>{
+        const navItems = document.querySelectorAll(".navItem")
+        navItems.forEach(Item => {
+            if (Item.classList.contains('active')){
+                Item.classList.remove('active')
+            }
+        });
+        ev.target.classList.add('active')
+    })
+    return navItem
+    
 
 
 
 }
-Sidebar()
+
+
+function content (){
+    let contentContainer = document.createElement('div')
+    return contentContainer
+}
+
+function sidebarContentContainer(){
+    let mainContainer = document.querySelector(".pageContainer")
+    let sidebarContentContainer = document.createElement('div')
+    sidebarContentContainer.classList.add('sidebarContent')
+    const sidebar = Sidebar()
+    console.log(sidebar);
+    const contentContainer = content()
+    sidebarContentContainer.appendChild(sidebar)
+    sidebarContentContainer.appendChild(contentContainer)
+    mainContainer.appendChild(sidebarContentContainer)    
+    
+}
+sidebarContentContainer()
